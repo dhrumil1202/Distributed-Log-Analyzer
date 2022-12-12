5ec8e493ed0a18a3b447df737c48eb575a3d5d9c
Abhinav <abve9575@colorado.edu>
2022-12-12 16:43:32 -0700

    Update IP

--- Client/app.py
+++ Client/app.py
@@ -9,6 +9,8 @@ import json
 app = Flask(__name__)
 #logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
+# Replace IP address with kubernetes IP when not running on local
+
 @app.route("/apiv1/add", methods=['GET','POST'])
 def add():
     data = request.json
@@ -25,7 +27,8 @@ def add():
         logs = "{0}".format(traceback.format_exc())
         dictobj = {"email": "dhrumildma@gmail.com", "logs": logs}
         data = json.dumps(dictobj)
-        response = requests.post("http://127.0.0.1:6000/apiv1/update/logs", data=data)
+        # Use kubernetes IP if not running on local
+        response = requests.post("http://127.0.0.1:5000/apiv1/update/logs", data=data)
         print(response)
     response = jsonify({'a': a, 'b': b, 'result': add})
     return response, 200
@@ -51,7 +54,8 @@ def div():
         logs = "{0}".format(traceback.format_exc())
         dictobj = {"email": "dhrumildma@gmail.com", "logs": logs}
         data = json.dumps(dictobj)
-        response = requests.post("http://127.0.0.1:6000/apiv1/update/logs", data=data)
+        # Use kubernetes IP if not running on local
+        response = requests.post("http://127.0.0.1:5000/apiv1/update/logs", data=data)
         print(response)
     response = jsonify({'a': a, 'b': b, 'result': div})
     return response, 200
@@ -72,7 +76,8 @@ def fileread():
         logs = "{0}".format(traceback.format_exc())
         dictobj = {"email": "dhrumildma@gmail.com", "logs": logs}
         data = json.dumps(dictobj)
-        response = requests.post("http://127.0.0.1:6000/apiv1/update/logs", data=data)
+        # Use kubernetes IP if not running on local
+        response = requests.post("http://127.0.0.1:5000/apiv1/update/logs", data=data)
         print(response)
     response = "hello"
     return response, 200
@@ -89,7 +94,8 @@ def unbound():
         logs = "{0}".format(traceback.format_exc())
         dictobj = {"email": "dhrumildma@gmail.com", "logs": logs}
         data = json.dumps(dictobj)
-        response = requests.post("http://127.0.0.1:6000/apiv1/update/logs", data=data)
+        # Use kubernetes IP if not running on local
+        response = requests.post("http://127.0.0.1:5000/apiv1/update/logs", data=data)
         print(response)
     return jsonify("Unbound"), 200
 
@@ -99,4 +105,4 @@ def unbound():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=6000, debug=True)
